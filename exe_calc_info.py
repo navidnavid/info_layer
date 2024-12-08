8440f90b43732e57d9f51d85a09161b00ddbaad2
navid navid <navid.otadi@gmail.com>
2024-12-08 17:27:09 +0100

    mayer info added

--- exe_calc_info.py
+++ exe_calc_info.py
@@ -23,23 +23,26 @@ class info_layer_content:
         self.arr_list.append( clasify_obj.classify_numbers_by_dis(arr_dvided_H_L[1]))
 
 
-
     def exe_devide_n_layer_info_content(self, arr) ->list[float]:
     # print(c_org_info)
         # cal fo devided arr
         info_obj = info()
         clasify_obj =clasify()
-
+        
+        self.info_list.append(f'layer: {0} ')
+        self.info_list.append(self.calc_info_content(arr))
         arr_dvided_H_L_inti = clasify_obj.classify_numbers_by_dis(arr)
         
         for i in range(self.loop):
             if(len(self.arr_list) == 0):
                 print(f"info-----> in init if .arr_list size is {len(self.arr_list)}")
+                self.info_list.append(f'layer: {1} ')
                 self.populate_lits(arr_dvided_H_L_inti)
             else:
                 print(f"info-----> in loop .arr_list size is {len(self.arr_list)}")
                 items=self.arr_list
                 self.arr_list = []
+                self.info_list.append(f'layer: {i+1} ')
                 for item in items:
                     self.populate_lits(item)
                     
