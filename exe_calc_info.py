95145d8081d9909358466dc7a1030016add93a74
navid navid <navid.otadi@gmail.com>
2024-12-08 16:52:31 +0100

    inof layer loop

--- exe_calc_info.py
+++ exe_calc_info.py
@@ -1,13 +1,51 @@
 from clasify_num import clasify
 from calc_info import info
 
-def exe_calss_info_deff(arr):
-    info_obj = info()
-    c_org_info = info_obj.info_content(arr)
-   # print(c_org_info)
-    # cal fo devided arr
-    clasify_obj =clasify()
-    class_H_L= clasify_obj.classify_numbers_by_dis(arr)
-    c1_info = info_obj.info_content(class_H_L[0])
-    c2_info = info_obj.info_content(class_H_L[1])
-    return ((c_org_info,c1_info,c2_info))
+class info_layer_content:
+    #todo move inof list and ar list as class memebr , n become calss member, then make iteartio over  exe...
+    def __init__(self, layer_num):
+        self.info_list = []
+        self.arr_list =[]
+        self.loop=layer_num
+
+    def calc_info_content(self, arr) ->float:
+        info_obj = info()
+        c_org_info = info_obj.info_content(arr) # cal max etp - etp 
+        return c_org_info
+    
+
+    def populate_lits(self, arr_dvided_H_L):
+        info_obj = info()
+        clasify_obj =clasify()
+        self.info_list.append(info_obj.info_content(arr_dvided_H_L[0]))
+        self.info_list.append(info_obj.info_content(arr_dvided_H_L[1]))
+        self.arr_list.append(clasify_obj.classify_numbers_by_dis(arr_dvided_H_L[0]))
+        self.arr_list.append( clasify_obj.classify_numbers_by_dis(arr_dvided_H_L[1]))
+
+
+
+    def exe_devide_n_layer_info_content(self, arr) ->list[float]:
+    # print(c_org_info)
+        # cal fo devided arr
+        info_obj = info()
+        clasify_obj =clasify()
+
+        arr_dvided_H_L_inti = clasify_obj.classify_numbers_by_dis(arr)
+        
+        for i in range(self.loop):
+            if(len(self.arr_list) == 0):
+                print(f"info-----> in init if .arr_list size is {len(self.arr_list)}")
+                self.populate_lits(arr_dvided_H_L_inti)
+            else:
+                print(f"info-----> in loop .arr_list size is {len(self.arr_list)}")
+                items=self.arr_list
+                self.arr_list = []
+                for item in items:
+                    self.populate_lits(item)
+                    
+            
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import numpy as np
-from exe_calc_info import exe_calss_info_deff
+from exe_calc_info import info_layer_content
 
 # Defining main function
 def main():
-    arr = [1,2,3,4,5,6,7,8,9,11,12]
+    arr = [1,2,3,4,5,6,7,8,9,11,12, 1,-2,-3,-4,5,6,7,-8,-9,11,-12]
     signal = np.array(arr)
-    info_diff_org_vs_divided_org = exe_calss_info_deff(signal)
+    info_layer_content_obj = info_layer_content(3)
+    info_layer_content_obj.exe_devide_n_layer_info_content(signal)
+    print( info_layer_content_obj.info_list)
 
 
 # Using the special variable 
